a8a67f8ad3b4265ee07e05baea33f5b062625876
Simon Johansson <simj91@gmail.com>
2014-09-22 14:18:27 +0200

    Adds constructors for array member types

--- types.go
+++ types.go
@@ -14,22 +14,60 @@ type Chord struct {
 	Id         int `db:id`
 	Cp_id      int `db:cp_id`
 	Root_an_id int `db:root_an_id`
+	Notes []*ChordNote
+}
+
+func NewChord(id, cp_id, root_an_id int) * Chord {
+	return &Chord {
+		Id: id,
+		Cp_id: cp_id,
+		Root_an_id: root_an_id,
+		Notes: make([]*ChordNote, 0),
+	}
 }
 
 type Scale struct {
 	Id         int `db:id`
 	Sp_id      int `db:sp_id`
 	Root_an_id int `db:root_an_id`
+	Notes []*ScaleNote
+}
+
+func NewScale(id, sp_id, root_an_id int) * Scale {
+	return &Scale {
+		Id: id,
+		Sp_id: sp_id,
+		Root_an_id: root_an_id,
+		Notes: make([]*ScaleNote, 0),
+	}
 }
 
 type ChordPattern struct {
 	Id   int    `db:id`
 	Name string `db:name`
+	Notes []*ChordPatternNote
+}
+
+func NewChordPattern(id int, name string) * ChordPattern {
+	return &ChordPattern {
+		Id: id,
+		Name: name,
+		Notes: make([]*ChordPatternNote, 0),
+	}
 }
 
 type ScalePattern struct {
 	Id   int    `db:id`
 	Name string `db:name`
+	Notes []*ScalePatternNote
+}
+
+func NewScalePattern(id int, name string) * ScalePattern {
+	return &ScalePattern {
+		Id: id,
+		Name: name,
+		Notes: make([]*ScalePatternNote, 0),
+	}
 }
 
 type ChordNote struct {
