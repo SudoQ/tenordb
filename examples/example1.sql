880fcd4607fab8b7ac7f8fdbc672c829767eaabe
Simon Johansson <simj91@gmail.com>
2014-09-22 21:53:05 +0200

    Adding example directory with a pilot sql example

---
+++ examples/example1.sql
@@ -0,0 +1,32 @@
+-- Select all chords which has atleast 3 notes in the C major scale
+SELECT
+absnote.name,
+cp.name,
+COUNT(cn.an_id) as count
+FROM
+chord
+JOIN
+chordnote as cn
+ON chord.id = cn.c_id
+JOIN
+chordpattern as cp
+ON chord.cp_id = cp.id
+JOIN
+absnote
+ON
+absnote.id = chord.root_an_id
+WHERE cn.an_id IN (
+	SELECT
+	sn.an_id
+	FROM
+	scale
+	JOIN
+	scalenote as sn
+	ON scale.id = sn.s_id
+	JOIN
+	scalepattern as sp
+	ON scale.sp_id=sp.id
+	WHERE scale.root_an_id = 0 AND sp.name = 'major'
+)
+GROUP BY chord.id, cp.name, absnote.name
+HAVING COUNT(cn.an_id) = 3;
