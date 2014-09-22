16f54518e5ebddd8a63d2982a8ace5cb41f02333
Simon Johansson <simj91@gmail.com>
2014-09-22 13:57:00 +0200

    Typos, cleanup and testing

--- gen/genContent.go
+++ gen/genContent.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"github.com/zephyyrr/goda"
+	"fmt"
 	tdb "github.com/SudoQ/tenordb"
 )
 
@@ -33,7 +34,7 @@ func init() {
 		storerMap["ScalePattern"],_ = dba.Storer("scalepattern", tdb.ScalePattern{})
 		storerMap["ChordNote"],_ = dba.Storer("chordnote", tdb.ChordNote{})
 		storerMap["ScaleNote"],_ = dba.Storer("scalenote", tdb.ScaleNote{})
-		storerMap["ChordPatternNote"],_ = dba.Storer("chordnatternnote", tdb.ChordPatternNote{})
+		storerMap["ChordPatternNote"],_ = dba.Storer("chordpatternnote", tdb.ChordPatternNote{})
 		storerMap["ScalePatternNote"],_ = dba.Storer("scalepatternnote", tdb.ScalePatternNote{})
         if err != nil {
                 panic(err)
@@ -44,17 +45,78 @@ func init() {
         }
 }
 
+
 func main() {
-	// Gen AbsNotes
-	chrom := []string {"C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"}
-	for i, name := range chrom {
-		err := storerMap["AbsNote"].Store(tdb.AbsNote{Id: i, Name: name})
+	var err error
+
+	tables := []string {
+		"chordnote",
+		"scalenote",
+		"chord",
+		"scale",
+		"chordpatternnote",
+		"scalepatternnote",
+		"chordpattern",
+		"scalepattern",
+		"absnote",
+		"relnote",
+	}
+
+	for _, table := range(tables) {
+		_, err = dba.Query(fmt.Sprintf("DELETE FROM %s;",table))
 		if err != nil {
 			log.Println(err)
 		}
+	}
+
+	// Gen Notes
+	chrom := []string {"C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"}
+	for i, name := range chrom {
+		err = storerMap["AbsNote"].Store(tdb.AbsNote{Id: i, Name: name})
+		if err != nil {
+			log.Println(err)
+		}
+		// TODO Find some relevant name for relative notes, like "major third"...
 		err = storerMap["RelNote"].Store(tdb.RelNote{Id: i, Name: ""})
 		if err != nil {
 			log.Println(err)
 		}
 	}
+
+
+	// Gen chord patterns
+	err = storerMap["ChordPattern"].Store(tdb.ChordPattern{
+		Id: 0,
+		Name: "Major",
+	})
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Gen chord pattern notes
+	cMajorNotes := []tdb.ChordPatternNote {
+		tdb.ChordPatternNote{
+			Cp_id: 0,
+			Rn_id: 0,
+		},
+		tdb.ChordPatternNote{
+			Cp_id: 0,
+			Rn_id: 4,
+		},
+		tdb.ChordPatternNote{
+			Cp_id: 0,
+			Rn_id: 7,
+		},
+	}
+
+	for _, note := range(cMajorNotes){
+		err = storerMap["ChordPatternNote"].Store(note)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
 }
