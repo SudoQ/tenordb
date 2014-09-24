bd07c4a122cb8ec833de3c33bc92804d8b2c14e8
Simon Johansson <simj91@gmail.com>
2014-09-24 13:41:08 +0200

    Test case for the chord assemble function

--- tenordb_test.go
+++ tenordb_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+/*
 func TestSetup(t *testing.T) {
 	log.Println("Testing Setup...")
 	err := Setup()
@@ -13,4 +14,16 @@ func TestSetup(t *testing.T) {
 		t.Fatal(err)
 	}
 	log.Println("Done!")
+}
+*/
+func TestAssembleChord(t *testing.T) {
+	log.Println("TestAssembleChord")
+	notes := []string{"C", "E", "G"}
+	chords, err := AssembleChord(notes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, chord := range(chords) {
+		log.Println(chord)
+	}
 }
